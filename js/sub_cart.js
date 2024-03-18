782a8579e8112893e019077c23aaab6efc8fdef3
Tanner <sonictanpat@gmail.com>
2024-03-18 10:31:09 -0500

    needs fixes, on 5e

---
+++ js/sub_cart.js
@@ -0,0 +1,45 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5, CSS3 and JavaScript 6th Edition
+   Tutorial 12
+   Case Problem 2
+
+   Author: 
+   Date:   
+
+   Filename: sub_cart.js
+
+
+   Functions List:
+   setupCart() 
+      Sets up the event handlers for the Add to Order buttons on the web page.
+      
+   addItem(e)
+      Adds the food item associated with the Add to Order button to the shopping
+      cart, keeping track of the number of items of each product ordered by 
+      the customer.
+
+*/
+
+window.addEventListener("load", setupCart);
+
+function setupCart() {
+   var addButton = document.getElementsByClassName("addButtons");
+   addEventHandler.createElement
+
+   for (var i = 0; i < addButton.length; i++) {
+      addEventListener("click", addItem)
+   }
+}
+
+function addItem() {
+   let node = document.getElementById("foodItem");
+   var foodID = getElementById(foodItem);
+   cloneNode() = "foodItem";
+   var cartBox = cartBox.getElementById;
+   var duplicateOrder = false;
+   for (var i = foodItem; i < cartBox.length; i++);
+
+   if (i = foodID + 1)
+}
